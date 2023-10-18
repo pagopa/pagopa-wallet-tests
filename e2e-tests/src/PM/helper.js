41b66d5aeff2d5bde86d1c1133ca82441864eb35
Fernando-Granato <fernando.granato@pagopa.it>
2023-10-18 15:12:46 +0200

    feat: added happy path for XPAY and VPOS

--- e2e-tests/src/PM/helper.js
+++ e2e-tests/src/PM/helper.js
@@ -1,3 +1,15 @@
+export const successfullyExecutionOnboarding = async (cardData, isXpay) => {
+    await fillCardForm(cardData);
+    await page.waitForNavigation();
+    if(isXpay){
+      await execute_mock_authorization_xpay();
+    } else{
+      await execute_mock_authorization_vpos();
+    }
+    await page.waitForNavigation();
+    const url = await page.url();
+    return url;
+};
 
 export const onboardCardMethodWithError4xx = async (cardData) => {
     await fillCardForm(cardData);
@@ -37,3 +49,79 @@ export const fillCardForm = async (cardData) => {
     const continueBtn = await page.waitForSelector(continueBtnXPath);
     await continueBtn.click();
 };
+
+const execute_mock_authorization_xpay = async() => {
+    const dataInput = '#otipee';
+    const confirmButton = 'button[name=btnAction]'
+    const mockOTPCode = '123456';
+    await page.waitForSelector(dataInput, {visible: true});
+    await page.click(dataInput);
+    await page.keyboard.type(mockOTPCode);
+    await page.waitForSelector(confirmButton);
+    await page.click(confirmButton);
+    await page.waitForNavigation();
+  }
+
+  const execute_mock_authorization_vpos = async() => {
+    const dataInput = '#challengeDataEntry';
+    const confirmButton = '#confirm'
+    const mockOTPCode = '123456';
+    const verificationStep = 2;
+  
+    for(let idx =0; idx < verificationStep;  idx++){
+      console.log(`executing vpos verification step: ${idx}`);
+      await page.waitForSelector(dataInput, {visible: true});
+      await page.click(dataInput);
+      await page.keyboard.type(mockOTPCode);
+  
+      await page.waitForSelector(confirmButton);
+      await page.click(confirmButton);
+    }
+  }
+
+export const retrieveValidRedirectUrl = async (pmHost) => {
+    const urlGetUser = `https://portal.test.pagopa.gov.it/pmmockserviceapi/cd/user/get`;
+    const responseGetUser = await fetch(urlGetUser, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    if(responseGetUser.status === 200){
+        const user = await responseGetUser.json();
+        const walletTokenCreditCard = user.sessionToken;
+        const urlPutUser = `https://portal.test.pagopa.gov.it/pmmockserviceapi/cd/user/save`;
+        const responsePutUser = await fetch(urlPutUser, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(user)
+        });
+        if(responsePutUser.status === 200){
+            const urlStartSession = `${pmHost}/payment-wallet/v1/wallets`;
+            const responseStartSession = await fetch(urlStartSession, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${walletTokenCreditCard}`
+              }
+            });
+            if(responseStartSession.status === 201){
+                const session = await responseStartSession.json();
+                return session.redirectUrl
+            }else{
+                console.log(responseStartSession.status)
+                throw Error("Error while start session");
+            }
+        } else {
+            throw Error("Error while saving user");
+        }
+       
+    }else{
+        throw Error("Error during user recovery");
+    }
+
+    
+}
