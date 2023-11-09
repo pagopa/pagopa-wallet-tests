8c1a9b890e6eb32b187da1a3bbe56470b9f01ca7
Michele Moio <michele.moio@protonmail.com>
2023-11-09 10:01:25 +0100

    test(CHK-2082): npg test retrieves redirectUrl from api

--- e2e-tests/src/NPG/helper.js
+++ e2e-tests/src/NPG/helper.js
@@ -1,10 +1,58 @@
+export const retrieveValidRedirectUrl = async (pmHost, paymentMethodId) => {
+  const urlGetUser = `https://portal.test.pagopa.gov.it/pmmockserviceapi/cd/user/get`;
+  const responseGetUser = await fetch(urlGetUser, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-export const fillCardDataForm = async (cardData) => {
+  if (responseGetUser.status === 200) {
+    const user = await responseGetUser.json();
+    const walletTokenCreditCard = user.sessionToken;
+    const urlPutUser = `https://portal.test.pagopa.gov.it/pmmockserviceapi/cd/user/save`;
+    const responsePutUser = await fetch(urlPutUser, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    });
+    if (responsePutUser.status === 200) {
+      const urlStartSession = `${pmHost}/payment-wallet/v1/wallets`;
+      const responseStartSession = await fetch(urlStartSession, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${walletTokenCreditCard}`,
+          'x-user-id': paymentMethodId,
+        },
+        body: JSON.stringify({
+          services: ['PAGOPA'],
+          useDiagnosticTracing: true,
+          paymentMethodId
+        }),
+      });
+      if (responseStartSession.status === 201) {
+        const session = await responseStartSession.json();
+        return session.redirectUrl;
+      } else {
+        throw Error('Error while start session');
+      }
+    } else {
+      throw Error('Error while saving user');
+    }
+  } else {
+    throw Error('Error during user recovery');
+  }
+};
+
+export const fillCardDataForm = async cardData => {
   const cardNumberInput = '#frame_CARD_NUMBER';
   const expirationDateInput = '#frame_EXPIRATION_DATE';
   const ccvInput = '#frame_SECURITY_CODE';
   const holderNameInput = '#frame_CARDHOLDER_NAME';
-  const continueBtnXPath = "button[type=submit]";
+  const continueBtnXPath = 'button[type=submit]';
   const disabledContinueBtnXPath = 'button[type=submit][disabled=""]';
   let iteration = 0;
   let completed = false;
@@ -14,25 +62,23 @@ export const fillCardDataForm = async (cardData) => {
     await page.waitForSelector(cardNumberInput, { visible: true });
     await page.click(cardNumberInput, { clickCount: 3 });
     await page.keyboard.type(cardData.number);
-    console.log("card number performed");
+    console.log('card number performed');
     await page.waitForSelector(expirationDateInput, { visible: true });
     await page.click(expirationDateInput, { clickCount: 3 });
     await page.keyboard.type(cardData.expirationDate);
-    console.log("expiration performed");
+    console.log('expiration performed');
     await page.waitForSelector(ccvInput, { visible: true });
     await page.click(ccvInput, { clickCount: 3 });
     await page.keyboard.type(cardData.ccv);
-    console.log("cvv performed");
+    console.log('cvv performed');
     await page.waitForSelector(holderNameInput, { visible: true });
     await page.click(holderNameInput, { clickCount: 3 });
     await page.keyboard.type(cardData.holderName);
-    console.log("holder performed");
+    console.log('holder performed');
     completed = !(await page.$(disabledContinueBtnXPath));
     await page.waitForTimeout(1_000);
   }
   const continueBtn = await page.waitForSelector(continueBtnXPath, { visible: true });
   await continueBtn.click();
   await page.waitForNavigation();
-
 };
-
--- e2e-tests/src/NPG/wallet.npg.onboarding.integration.test.ts
+++ e2e-tests/src/NPG/wallet.npg.onboarding.integration.test.ts
@@ -1,20 +1,17 @@
-import { fillCardDataForm } from './helper';
+import { fillCardDataForm, retrieveValidRedirectUrl } from './helper';
 
 describe('wallet payment npg activation tests', () => {
   /**
    * Test input and configuration
    */
-  const NPG_WALLET_URL = String(process.env.NPG_WALLET_URL);
+  const PM_HOST = String(process.env.PM_HOST);
+  const PAYMENT_METHOD_ID = String(process.env.PAYMENT_METHOD_ID);
   const VALID_CARD_DATA = {
     number: String(process.env.CARD_NUMBER_XPAY),
     expirationDate: String(process.env.CARD_EXPIRATION_DATE_XPAY),
     ccv: String(process.env.CARD_CCV_XPAY),
     holderName: String(process.env.CARD_HOLDER_NAME_XPAY),
   };
-
-  const sessionToken = 'exampleToken';
-  const walletId = '123';
-  const NPG_VALID_URL = `${NPG_WALLET_URL}#sessionToken=${sessionToken}&walletId=${walletId}`;
 
   /**
    * Increase default test timeout (60000ms)
@@ -26,14 +23,12 @@ describe('wallet payment npg activation tests', () => {
   page.setDefaultNavigationTimeout(timeout);
   page.setDefaultTimeout(timeout);
 
-  beforeEach(async () => {
-    await page.goto(NPG_VALID_URL);
-  });
-
   it('It Should redirect with outcome 0 on success', async () => {
     /*
      * 1. Payment with valid notice code
      */
+    const redirectUrl = await retrieveValidRedirectUrl(PM_HOST, PAYMENT_METHOD_ID);
+    await page.goto(redirectUrl);
     await fillCardDataForm(VALID_CARD_DATA);
 
     expect(page.url()).toContain('gdi-check');
