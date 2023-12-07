8ce9fe20ca423631f79cf3f648a7a15787c67a69
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2023-12-07 12:40:24 +0100

    feat(CHK-2172): updating credit card npg onboarding e2e test

--- e2e-tests/src/NPG/wallet.npg.onboarding.integration.test.ts
+++ e2e-tests/src/NPG/wallet.npg.onboarding.integration.test.ts
@@ -1,18 +1,35 @@
 import { fillCardDataForm, retrieveValidRedirectUrl } from './helper';
 
-describe('wallet payment npg activation tests', () => {
-  /**
-   * Test input and configuration
-   */
-  const WALLET_HOST = String(process.env.WALLET_HOST);
-  const PAYMENT_METHOD_ID = String(process.env.PAYMENT_METHOD_ID);
-  const VALID_CARD_DATA = {
-    number: String(process.env.CARD_NUMBER_XPAY),
-    expirationDate: String(process.env.CARD_EXPIRATION_DATE_XPAY),
-    ccv: String(process.env.CARD_CCV_XPAY),
-    holderName: String(process.env.CARD_HOLDER_NAME_XPAY),
-  };
+/**
+ * This function is usefull when we need to wait for the puppeter page instance get a certain
+ * url value based on the inclusion of the urlSubstring parameter
+ */
+export const waitUntilUrlContains = async (urlSubstring) => 
+  await page.waitForFunction(`window.location.href.includes("${urlSubstring}")`);
 
+/**
+ * This function wait for obtain the outcome parameter from the final result url
+ * when the gdi check phase, the 3ds challenge and esito phase ends
+ * @returns number
+ */
+const getOutcome = async () => {
+  try {
+    expect(page.url()).toContain('/gdi-check');
+    await waitUntilUrlContains("/esito");
+    expect(page.url()).toContain('/esito');
+    await waitUntilUrlContains("/outcomes");
+    const url = new URL(page.url());
+    const outcome = new URLSearchParams(url.search).get("outcome");
+    if( outcome === null) return -1
+    return parseInt(outcome)
+  } catch {
+    return -1
+  }
+}
+
+describe('Credit Card Wallet: onboarding with NPG', () => {
+  const WALLET_HOST = String(process.env.WALLET_HOST);
+  const PAYMENT_METHOD_ID = String(process.env.CREDIT_CARD_PAYMENT_METHOD_ID);
   /**
    * Increase default test timeout (60000ms)
    * to support entire payment flow
@@ -23,16 +40,21 @@ describe('wallet payment npg activation tests', () => {
   page.setDefaultNavigationTimeout(timeout);
   page.setDefaultTimeout(timeout);
 
-  it('It Should redirect with outcome 0 on success', async () => {
+
+  it('should redirect with outcome 0 (success) success using an valid visa card', async () => {
     /*
      * 1. Payment with valid notice code
      */
+    const VALID_VISA_CARD_DATA = {
+      number: '4012000000020089',
+      expirationDate: '1226',
+      ccv: '123',
+      holderName: "TEST TEST",
+    };
     const redirectUrl = await retrieveValidRedirectUrl(WALLET_HOST, PAYMENT_METHOD_ID);
     await page.goto(redirectUrl);
-    await fillCardDataForm(VALID_CARD_DATA);
-
-    expect(page.url()).toContain('gdi-check');
-    await page.waitForNavigation();
-    expect(page.url()).toContain('outcome=0');
+    await fillCardDataForm(VALID_VISA_CARD_DATA);
+    const outocome = await getOutcome()
+    expect(outocome).toBe(0)
   });
 });
