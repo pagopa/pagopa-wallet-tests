eeb0b3997fc06128e9e553d340711c52657c03fa
Michele Moio <michele.moio@protonmail.com>
2024-01-08 17:17:28 +0100

    fix(CHK-2279): renames payment secret env

--- e2e-tests/src/NPG/wallet.npg.payment.integration.test.js
+++ e2e-tests/src/NPG/wallet.npg.payment.integration.test.js
@@ -1,7 +1,7 @@
 import {retrievePaymentRedirectUrl} from "./helper";
 
 const WALLET_HOST = String(process.env.WALLET_HOST);
-const WALLET_PAYMENT_TOKEN = String(process.env.WALLET_PAYMENT_TOKEN);
+const USER_WALLET_TOKEN = String(process.env.USER_WALLET_TOKEN);
 
 describe('wallet npg payment check tests', () => {
   /**
@@ -16,7 +16,7 @@ describe('wallet npg payment check tests', () => {
 
   it('It Should redirect with outcome 0 on success', async () => {
 
-    const authorizationUrl = await retrievePaymentRedirectUrl(WALLET_HOST, WALLET_PAYMENT_TOKEN);
+    const authorizationUrl = await retrievePaymentRedirectUrl(WALLET_HOST, USER_WALLET_TOKEN);
     console.debug(authorizationUrl);
     await page.goto(authorizationUrl);
 
